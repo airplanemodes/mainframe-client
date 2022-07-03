d44dd93a372a64b78d0406b4d937732408418139
Roy Elman <airplanemodes@Roys-MacBook-Pro.local>
2022-07-03 23:06:51 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header';
 import Menu from './components/Menu';
 import Feed from './components/Feed';
+import Footer from './components/Footer';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Menu />
       <Feed />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import react from 'react';
+
+function Footer() {
+    return (
+        <p>Mainframe (c) {new Date().getFullYear()}</p>
+    )
+}
+
+export default Footer
