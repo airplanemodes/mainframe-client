43cda5fd131288f118c873ee07e9ebc00098c142
Roy Elman <airplanemodesnetwork@gmail.com>
2022-07-09 19:16:24 +0300

    posts

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,11 +1,35 @@
-function Feed() {
+import React, { useState, useEffect } from 'react';
+import { api_method, api_url } from '../services/api-service';
+
+function Feed(props) {
+
+    let [arr, setArr] = useState([]);
+
+    useEffect(() => {
+        getFeed();
+    }, [props.location]);
+
+    const getFeed = async() => {
+        let url = api_url + '/posts';
+        let data = await api_method(url, 'GET');
+
+        console.log(data);
+        data.reverse();
+        setArr(data);
+    }
+
     return (
         <article>
             <section className="feedBox">
                 <h2>Posts feed</h2>
-                    <ul>
-                        <li>Post 1</li>
-                    </ul>
+                    {arr.map((item, i) => {
+                        return (
+                            <div key={item._id}>
+                                <h3>{item.postName}</h3>
+                                <p>{item.postText}</p>
+                            </div>
+                        )
+                    })}
             </section>
         </article>
     )
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 function Footer() {
     return (
         <footer>Mainframe &copy; {new Date().getFullYear()}</footer>
