fd59003959e9f766b77d3b0e31d58fcc3ed89742
Roy Elman <airplanemodesnetwork@gmail.com>
2022-07-15 20:09:47 +0300

    users

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,15 +1,38 @@
 import React from "react";
+import { useForm } from 'react-hook-form';
+import { api_url, api_method } from '../services/api-service';
 
 function Signup() {
+
+    let { register, handleSubmit, formState: { errors } } = useForm();
+
+    let usernameRef = register('username', { required:true, minLength: 4 });
+    let passwordRef = register('password', { required:true, minLength: 4 });
+
+    const onFormSubmit = async(formData) => {
+        console.log(formData);
+        let url = api_url + '/users';
+
+        try {
+            let data = await api_method(url, 'POST', formData);
+            if (data._id) {
+                alert('Success!');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
-        <div>
+        <form onSubmit={handleSubmit(onFormSubmit)}>
             <label>Username</label>
-            <input></input>
+            <input {...usernameRef} type='text'></input>
             <br />
             <label>Password</label>
-            <input></input>
+            <input {...passwordRef} type='password'></input>
             <br />
-        </div>
+            <button className="darkButton">Submit</button>
+        </form>
     )
 }
 
