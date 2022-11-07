1c696a83a6fde86de8b8dc46ce1020ce97178074
Roy Elman <airplanemodesnetwork@gmail.com>
2022-11-07 14:16:01 +0200

    edit component

--- src/components/edit.js
+++ src/components/edit.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import { axiosRequest, serverAddress } from '../services/api';
 import './styles/edit.css';
 
@@ -7,28 +8,66 @@ export default function Edit() {
 
     const { id } = useParams();
     const [ entry, setEntry ] = useState({});
-
+    
     const getEntryForEditing = async() => {
         const url = serverAddress+'/edit/'+id;
         const data = await axiosRequest(url);
         console.log(data);
         setEntry(data);
     }
-
+    
     useEffect(() => {
         getEntryForEditing();
+        // eslint-disable-next-line
     },[]);
+    
+    const { register, handleSubmit } = useForm();
+
+    let titleRef = register('title', { required: true, maxLength: 60 });
+    let nodeRef = register('node', { required: true, maxLength: 32 });
+    let contentRef = register('content', { required: true, maxLength: 65536 });
+
+    const submitEdit = async(formdata) => {        
+        try {
+            formdata.author = entry.author;
+            formdata.created = entry.created;
+            formdata.id = entry.id;
+            formdata.points = entry.points;
+            const url = serverAddress+"/entries/"+formdata.id;
+            console.log(formdata);
+            // console.log(url);
+            let response = await axiosRequest(url, 'PUT', formdata);
+            console.log(response);
+            window.location = '/main';
+        } catch (error) {
+            console.log(error);
+        };
+    };
 
     return (
         <div id='edit'>
-            <h3 id='entryHeader'>{entry.title}</h3>
-            <div id='entryContent'>{entry.content}</div>
-            {/* <div id="entryDate">{entry.created}</div> */}
-            <div id='entrySignature'>
-                <a href={'/users/'+entry.author}>{entry.author}</a>
-                @ {entry.node} {entry.created}
-            </div>
-            <a href='/main'><button id='entryReturn'>Return 0</button></a>
+            <h3 id='entryHeader'>Entry editing</h3>
+            <form id='editForm' onSubmit={handleSubmit(submitEdit)}>
+                <label>Title</label>
+                <input type={'text'} {...titleRef} defaultValue={entry.title}/>
+                <br />
+                <br />
+                <label>Node</label>
+                <select {...nodeRef}>
+                    { entry.node === "code" ? <option selected>code</option> : <option>code</option> }
+                    { entry.node === "network" ? <option selected>network</option> : <option>network</option> }
+                    { entry.node === "hack" ? <option selected>hack</option> : <option>hack</option> }
+                    { entry.node === "society" ? <option selected>society</option> : <option>society</option> }
+                </select>
+                <br />
+                <br />
+                <label>Content</label>
+                <br />
+                <textarea rows={12} {...contentRef} defaultValue={entry.content}/>
+                <br />
+                <button>Edit</button>
+            </form>
+            <a href='/main'><button id='editReturn'>Return 0</button></a>
         </div>
     );
 };
