52a639f6f0de6e7cf3d039e322054725249b79c6
Roy Elman <airplanemodesnetwork@gmail.com>
2023-07-25 08:32:53 +0300

    upd: write screen css

--- src/components/write.js
+++ src/components/write.js
@@ -7,70 +7,64 @@ import './styles/write.css';
 
 export default function Write() {
 
-  let [ user, setUser ] = useState();
+    let [ user, setUser ] = useState();
 
-  const initializeUser = async() => {
-    let userinit = await userdataUpdate();
-    // console.log(userinit);
-    if (userinit.username) {
-      setUser(userinit);
-    } else {
-      window.location = '/main';
+    const initializeUser = async() => {
+        let userobject = await userdataUpdate();
+        // console.log(userobject);
+        if (userobject.username) setUser(userobject);
+        else window.location = '/main';
     }
-  }
 
-  useEffect(() => {
-    initializeUser();
-  },[]);
+    useEffect(() => {
+        initializeUser();
+    },[]);
 
-  const { register, handleSubmit } = useForm();
+    const { register, handleSubmit } = useForm();
 
-  let titleRef = register('title', { required: true, maxLength: 60 });
-  let nodeRef = register('node', { required: true, maxLength: 32 });
-  let contentRef = register('content', { required: true, maxLength: 65536 });
+    let titleRef = register('title', { required: true, maxLength: 60 });
+    let nodeRef = register('node', { required: true, maxLength: 32 });
+    let contentRef = register('content', { required: true, maxLength: 65536 });
 
-  const writeEntry = async(formdata) => {
-    try {
-      const dateObj = new Date();
-      formdata.created = `${dateObj.getFullYear()}-${dateObj.getMonth()+1}-${dateObj.getDate()}`;
-      formdata.author = user.username;
-      formdata.points = 0;
-      const url = serverAddress+'/entries';
-      await axiosRequest(url, 'POST', formdata);
-      alert("Entry was written to the database");
-      window.location = '/main';
-    } catch (error) {
-      console.log(error);
+    const writeEntry = async(formdata) => {
+        try {
+            const dateObj = new Date();
+            formdata.created = `${dateObj.getFullYear()}-${dateObj.getMonth()+1}-${dateObj.getDate()}`;
+            formdata.author = user.username;
+            formdata.points = 0;
+            const url = serverAddress+'/entries';
+            await axiosRequest(url, 'POST', formdata);
+            alert("Entry was written to the database");
+            window.location = '/main';
+        } catch (error) {
+            console.log(error);
+        }
     }
-  }
 
-  return (
-    <div id='write'>
-      <h3 id='write-header'>Write a new entry</h3>
-      <form id='write-form' onSubmit={handleSubmit(writeEntry)}>
-        {/* TODO: add table */}
-        <label>Title</label>
-        <input type={'text'} {...titleRef} />
-        <br />
-        <br />
-        <label>Node</label>
-        <select {...nodeRef}>
-          <option>code</option>
-          <option>network</option>
-          <option>hack</option>
-          <option>society</option>
-        </select>
-        <br />
-        <br />
-        <label>Content</label>
-        <br />
-        <textarea rows={12} {...contentRef}/>
-        <br />
-        <button className='bar-button'>Write</button>
-      </form>
-      <div id='write-return-div'>
-        <ReturnLight />
-      </div>
-    </div>
-  );
+    return (
+        <div id='write'>
+            <h3 id='write-header'>Write a new entry</h3>
+            <form id='write-form' onSubmit={handleSubmit(writeEntry)}>
+                <label>Title</label>
+                <input type={'text'} {...titleRef} />
+                <br />
+                <label id='node-label'>Node</label>
+                <select {...nodeRef}>
+                    <option>code</option>
+                    <option>network</option>
+                    <option>hack</option>
+                    <option>society</option>
+                </select>
+                <br />
+                <label>Content</label>
+                <br />
+                <textarea rows={12} {...contentRef}/>
+                <br />
+                <button id='send-write' className='bar-button'>Write</button>
+            </form>
+            <div id='write-return'>
+                <ReturnLight />
+            </div>
+        </div>
+    );
 }
